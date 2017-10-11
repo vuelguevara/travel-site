531ec599180fcebe78991e191ca5475828f991ac
vuelguevara <vuelguevara@gmail.com>
2017-10-11 22:14:50 +0800

    Added gulp watch

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+
+gulp.task('default', function() {
+    console.log("Hooray - you created a Gulp Task")
+});
+
+gulp.task('html',function(){
+    console.log("Imagine something useful being done to your HTML here.")
+});
+
+gulp.task('styles',function(){
+    console.log("Imagine SASS or PostCSS tasks running here.")
+});
+
+gulp.task('watch', function() {
+    
+    watch('./app/index.html' , function()
+    {
+        gulp.start('html');
+    });
+
+    watch('./app/assets/styles/**/*.css' , function()
+    {
+        gulp.start('styles');
+    });
+    
+});
+    
