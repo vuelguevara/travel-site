5b5719a8e65ef065790b83ea0b52e1eb41b78b73
vuelguevara <vuelguevara@gmail.com>
2017-10-22 17:32:16 +0800

    arrange file structure and added CSS styling

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
+var cssImport = require('postcss-import');
 
 gulp.task('default', function() {
     console.log("Hooray - you created a Gulp Task")
@@ -15,7 +16,7 @@ gulp.task('html',function(){
 
 gulp.task('styles',function(){
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssvars, nested, autoprefixer]))
+        .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
         .pipe(gulp.dest('./app/temp/styles'));
 });
 
