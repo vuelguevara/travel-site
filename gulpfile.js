40ab3eecb73752d43d9e1c48a31d9c8bd359af8c
Vuel <vuelguevara@gmail.com>
2017-10-23 18:17:39 +0800

    Added browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var autoprefixer = require('autoprefixer');
 var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
 var cssImport = require('postcss-import');
+var browserSync = require('browser-sync').create();
 
 gulp.task('default', function() {
     console.log("Hooray - you created a Gulp Task")
@@ -21,16 +22,28 @@ gulp.task('styles',function(){
 });
 
 gulp.task('watch', function() {
+
+    browserSync.init({
+        notify: false,
+        server: {
+            baseDir: "app"
+        }
+    })
     
     watch('./app/index.html' , function()
     {
-        gulp.start('html');
+        browserSync.reload();
     });
 
     watch('./app/assets/styles/**/*.css' , function()
     {
-        gulp.start('styles');
+        gulp.start('cssInject');
     });
     
 });
+
+gulp.task('cssInject', ['styles'], function(){
+    return gulp.src('./app/temp/styles/styles.css')
+        .pipe(browserSync.stream());
+})
     
