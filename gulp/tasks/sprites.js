761737812095ff799d725db691ba500e21ca9283
vuelguevara <vuelguevara@gmail.com>
2017-11-05 00:06:57 +0800

    Added gulp-sprite automation

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,9 +1,11 @@
 var gulp = require ('gulp');
 var svgSprite = require('gulp-svg-sprite');
+var rename = require('gulp-rename');
 
 var config = {
     mode: {
         css: {
+            sprite: 'sprite.svg',
             render: {
                 css: {
                     template: './gulp/templates/sprite.css'
@@ -20,3 +22,16 @@ gulp.task('createSprite', function(){
         .pipe (svgSprite(config))
         .pipe(gulp.dest('./app/temp/sprite/'));
 });
+
+gulp.task('copySpriteGraphic', ['createSprite'], function(){
+    return gulp.src('./app/temp/sprite/css/**/*.svg')
+        .pipe (gulp.dest('./app/assets/images/sprites'));
+});
+
+gulp.task('copySpriteCSS', ['createSprite'], function(){
+    return gulp.src('./app/temp/sprite/css/*.css')
+        .pipe(rename('_sprite.css'))
+        .pipe(gulp.dest('./app/assets/styles/modules'));
+});
+
+gulp.task('icons', ['createSprite', 'copySpriteGraphic', 'copySpriteCSS']); 
