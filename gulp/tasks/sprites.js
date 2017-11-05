3b2362cb7349d869a005404d18cd1ee25abf21b8
vuelguevara <vuelguevara@gmail.com>
2017-11-06 00:01:11 +0800

    Completed auto sprite generation

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,6 +1,7 @@
 var gulp = require ('gulp');
 var svgSprite = require('gulp-svg-sprite');
 var rename = require('gulp-rename');
+var del = require ('del');
 
 var config = {
     mode: {
@@ -17,7 +18,11 @@ var config = {
 
 }
 
-gulp.task('createSprite', function(){
+gulp.task('beginClean', function() {
+    return del (['./app/temp/sprite/', './app/assets/images/sprites']);
+});
+
+gulp.task('createSprite', ['beginClean'], function(){
     return gulp.src('./app/assets/images/icons/**/*.svg')
         .pipe (svgSprite(config))
         .pipe(gulp.dest('./app/temp/sprite/'));
@@ -34,4 +39,8 @@ gulp.task('copySpriteCSS', ['createSprite'], function(){
         .pipe(gulp.dest('./app/assets/styles/modules'));
 });
 
-gulp.task('icons', ['createSprite', 'copySpriteGraphic', 'copySpriteCSS']); 
+gulp.task('endClean', ['copySpriteGraphic', 'copySpriteCSS'],function(){
+    return del (['./app/temp/sprite/']);
+});
+
+gulp.task('icons', ['beginClean', 'createSprite', 'copySpriteGraphic', 'copySpriteCSS', 'endClean']); 
